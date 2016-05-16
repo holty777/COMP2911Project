6378dff0485bf41fa1e3f64d0a8a231b1bf65902
nickbalnaves@outlook.com <nickbalnaves@gmail.com>
2016-05-16 14:31:04 +1000

    started to configure right panel

--- src/MazePuzzleGame.java
+++ src/MazePuzzleGame.java
@@ -1,6 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class MazePuzzleGame implements Runnable {
@@ -11,6 +14,7 @@ public class MazePuzzleGame implements Runnable {
 	private JFrame mainFrame;
 	private MenuPanel menuPanel;
 	private GameEngine gameEngine;
+	private JPanel homeGlassPane;
 	
 	//maybe change this to gamePanel
 	private MainWindow mazeWindow;
@@ -21,13 +25,15 @@ public class MazePuzzleGame implements Runnable {
 		this.gameEngine = ge;
 		
 		mainFrame = new JFrame("Maze Puzzle Game");
+		mainFrame.setSize(1280, 720);
+		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//need to use setPreferredSize for these
-		menuPanel = new MenuPanel(this);
+		menuPanel = new MenuPanel();
+		menuPanel.setPreferredSize(new Dimension(250, 700));
 		//mazeWindow = new MainWindow();
 		maze = new Maze();
-		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -44,7 +50,29 @@ public class MazePuzzleGame implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		if (homeGlassPane != null) {
+			mainFrame.setGlassPane(homeGlassPane);
+			homeGlassPane.setOpaque(false);
+			homeGlassPane.setVisible(true);
+		}
+		mainFrame.getContentPane().add(menuPanel, BorderLayout.EAST);
+		//mainFrame.getContentPane().add(simulationPanel, BorderLayout.WEST);
+		mainFrame.setResizable(false);
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		
+		while (true) {
+			// start new simulation if game is not on
+			/*if (!gameEngine.isInGame() && mainFrame.isVisible()) {
+				simulationPanel.startSimulationGame();
+			}*/
+			// delay waiting
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}
 		
 	}
 }
--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,8 +1,59 @@
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuPanel {
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-	public MenuPanel(MazePuzzleGame mazePuzzleGame) {
-		// TODO Auto-generated constructor stub
+public class MenuPanel extends JPanel {
+
+	public MenuPanel() {
+
+		setLayout(new GridBagLayout());
+
+		setOpaque(true);
+		setBackground(Color.GRAY);
+
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.weightx = 1;
+		gc.weighty = 10;
+		gc.fill = GridBagConstraints.BOTH;
+
+		// Button for Single Player
+		gc.gridx = 0;
+		gc.gridy = 0;
+
+		JButton singlePlayerButton = new JButton("Single Player");
+
+		add(singlePlayerButton, gc);
+
+		// Button for Two Player
+		gc.gridy = 1;
+		JButton twoPlayersButton = new JButton("Double Player");
+		add(twoPlayersButton, gc);
+
+		// how to play button
+		gc.gridy = 2;
+		JButton htpButton = new JButton("Instructions");
+		add(htpButton, gc);
+
+		// Button for Quit
+		gc.gridy = 3;
+		JButton quitButton = new JButton("Quit");
+		add(quitButton, gc);
+		quitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+
+		});
 	}
 
+	public static void closeJOptionPanel() {
+		JOptionPane.getRootFrame().dispose();
+	}
 }
