aaedbc751277d30dba89cc85c4d72f2454349c05
Jesse-Moses <jesse@nichetechnical.com.au>
2016-05-19 16:16:34 +1000

    Create PredatorState.java

---
+++ src/PredatorState.java
@@ -0,0 +1,25 @@
+
+public class PredatorState {
+	private int row;
+	private int col;
+	private PredatorState previousState = null;
+	
+	public PredatorState(int row, int col, PredatorState prev){
+		this.row = row;
+		this.col = col;
+		this.previousState = prev;
+		
+	}
+	
+	public int getRow(){
+		return this.row;
+	}
+	
+	public int getCol(){
+		return this.col;
+	}
+	
+	public PredatorState getPreviousState(){
+		return this.previousState;
+	}
+}
