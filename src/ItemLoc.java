5075f6c1175e7a0046a3252b5e187bf7bec3b3a9
stev3bb <s.liang@student.unsw.edu.au>
2016-05-19 16:57:48 +1000

    Create ItemLoc.java
    added item location state

---
+++ src/ItemLoc.java
@@ -0,0 +1,13 @@
+
+public class ItemLoc {
+	private int x;
+	private int y;
+	private int itemType;
+	
+	public ItemLoc (int x, int y, int item){
+		this.x = x;
+		this.y = y;
+		this.itemType = item;
+	}
+	
+}
